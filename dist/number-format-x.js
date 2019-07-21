5d641c409b2e11b3ad829b1d15e7299432ebe69a
Graham Fairweather <xotic750@gmail.com>
2019-07-22 01:43:09 +0200

    :bookmark: v4.0.3
    Auto build

--- dist/number-format-x.js
+++ dist/number-format-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-19T23:12:06.266Z",
+  "date": "2019-07-21T23:43:04.010Z",
   "describe": "",
   "description": "Format a number.",
   "file": "number-format-x.js",
   "hash": "48b987c94c09f9f2d55a",
   "license": "MIT",
-  "version": "4.0.2"
+  "version": "4.0.3"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
