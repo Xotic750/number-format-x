c81183f8b8da80593efbc4d8bfabe83633846338
Graham Fairweather <xotic750@gmail.com>
2019-08-07 04:09:21 +0200

    Auto build

--- dist/number-format-x.js
+++ dist/number-format-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-06T21:56:17.316Z",
+  "date": "2019-08-07T02:09:15.894Z",
   "describe": "",
   "description": "Format a number.",
   "file": "number-format-x.js",
